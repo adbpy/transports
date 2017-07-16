5ac42b9774b2d2596910dac5a3b6d745dd839cb4
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-16 15:35:14 -0700

    Include __init__.py in isort and verbose logging to see skips.

--- adbtp/tcp/__init__.py
+++ adbtp/tcp/__init__.py
@@ -9,7 +9,6 @@ import socket
 
 from .. import transport
 
-
 #: Function partial that sets the default transport operation timeout to the
 #: default socket timeout value and convert it from seconds to milliseconds.
 transport_timeout = functools.partial(transport.transport_timeout,  # pylint: disable=invalid-name
