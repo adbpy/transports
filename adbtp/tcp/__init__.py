7650ebf398929b0d9220ceff9cef59a91a5f348b
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-16 13:20:37 -0700

    Disable invalid-name for transport_timeout partial.

--- adbtp/tcp/__init__.py
+++ adbtp/tcp/__init__.py
@@ -12,6 +12,6 @@ from .. import transport
 
 #: Function partial that sets the default transport operation timeout to the
 #: default socket timeout value and convert it from seconds to milliseconds.
-transport_timeout = functools.partial(transport.transport_timeout,
+transport_timeout = functools.partial(transport.transport_timeout,  # pylint: disable=invalid-name
                                       default=socket.getdefaulttimeout(),
                                       seconds=True)
