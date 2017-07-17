e84ce321a20695a056e205482f2b9ea194267018
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-16 17:14:34 -0700

    Add stub tests module for exceptions.

---
+++ tests/test_exceptions.py
@@ -0,0 +1,14 @@
+"""
+    test_exceptions
+    ~~~~~~~~~~~~~~~
+
+    Tests for the :mod:`~adbtp.exceptions` module.
+"""
+import pytest
+
+from adbtp import exceptions
+
+
+@pytest.mark.xfail(reason='Not implemented')
+def test_stub():
+    assert False
