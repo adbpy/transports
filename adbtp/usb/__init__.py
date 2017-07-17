26c58b548930b9ec109b453a79798f6f60e75a57
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-16 17:24:55 -0700

    Add function partial for USB transport default timeout of 0 ms.

--- adbtp/usb/__init__.py
+++ adbtp/usb/__init__.py
@@ -4,3 +4,9 @@
 
     Package that contains Universal Serial Bus (USB) transports.
 """
+import functools
+
+from .. import transport
+
+#: Function partial that sets the default USB transport operation timeout to zero milliseconds.
+transport_timeout = functools.partial(transport.transport_timeout, default=0)  # pylint: disable=invalid-name
