c5240f98d299f6ceee9edd3f107ebfdd3363400a
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-16 17:15:26 -0700

    Add stub tests module for transport.

---
+++ tests/test_transport.py
@@ -0,0 +1,14 @@
+"""
+    test_transport
+    ~~~~~~~~~~~~~~
+
+    Tests for the :mod:`~adbtp.transport` module.
+"""
+import pytest
+
+from adbtp import transport
+
+
+@pytest.mark.xfail(reason='Not implemented')
+def test_stub():
+    assert False
