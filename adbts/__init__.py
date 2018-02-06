05f30f0c4cd774a9eadff7f8e46d908356b8b8ec
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-02-05 22:35:09 -0800

    Expose transport module to public API.

--- adbts/__init__.py
+++ adbts/__init__.py
@@ -10,6 +10,9 @@ from . import exceptions
 from .exceptions import *
 from . import timeouts
 from .timeouts import *
+from . import transport
+from .transport import *
+from . import tcp, usb
 
-__all__ = exceptions.__all__ + timeouts.__all__
+__all__ = exceptions.__all__ + transport.__all__ + timeouts.__all__ + ['tcp', 'usb']
 __version__ = '0.0.1'
